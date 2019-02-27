f24775d7cf8250c6369edc67841c9bb713cb4081
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-02-27 19:32:07 +0530

    set 1-8.c

---
+++ set1-8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+char a[100];
+int n,i;
+scanf["[/t/ns",a);
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a(0)>90)
+{
+a[0]=a[0]_32;
+}
+else if(a(i+1)>90)
+{
+a[i+1}=a[i+1]_32;
+}
+}
+}
+printf("%s",a);
+return 0;
+}
