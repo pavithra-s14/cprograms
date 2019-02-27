52299056a8b1a8bc887c654a018c0575ecabba34
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-02-27 20:17:07 +0530

    set1 -9.c

---
+++ set1 -9,.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main();
+{
+int n1,n2,i,j,flag-0,count=0;
+for(i=n1;i<n2;i++);
+{
+flag=0;
+for(j-=2;j<=i/2;i++)
+{
+if(i/j)==0)
+{
+flag=1;
+break;
+}
+else
+{
+flag=0;
+}
+if(flag==0)
+{
+count++;
+}
+}
+printf("%d",cont);
+return 0;
+}
