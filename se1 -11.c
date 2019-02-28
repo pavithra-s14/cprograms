74f571c8843cc0292e638066e4538ea4dccf099b
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-02-28 20:11:14 +0530

    set1-11.c

---
+++ se1 -11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+imt main(void);
+{
+char str[50];
+scanf("%s",str);
+if(str[0]=='s'||str[0]='s');
+{
+printf("/n yes");
+else
+{
+printf("/n no);
+}
+return 0;
+}
