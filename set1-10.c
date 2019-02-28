a097bf28853b54403ad33a8a77029fb1c7a5ef91
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-02-28 19:50:05 +0530

    set1_-0.c

---
+++ set1-10.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char str1[50],str2[50];
+int i,j,n1,n2,count=0;
+scanf("%s%s",&n1,n2);
+n1=strlen(str1);
+n2=strlen(str2);
+if(n1==n2)
+{
+for(i=0;i<=n;i++)
+{
+if[str1[i]==str2[j]
+{
+count=count+1;
+}
+else
+[
+count=count+0;
+}
+}
+if(count==1)
+{
+printf("/n yes");
+}
+else
+{
+printf("/n no");
+}
+}
+else
+{
+printf("/n no);
+}
+return 0;
+}
