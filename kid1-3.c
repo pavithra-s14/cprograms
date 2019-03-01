a581b8f13782f237df009ff08299b9bab6b09515
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-01 20:04:25 +0530

    kid1-3.c

---
+++ kid1-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int ch;
+scanf("%c",&ch);
+if(ch==||'a' ch==||='e' ch==||'i' ch==||'o' ch==||'u')
+{
+printf("is a vowel");
+}
+else
+{
+printf("is  a constant")
+}
