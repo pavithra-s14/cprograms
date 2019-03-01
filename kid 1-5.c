cf638f0c9b4bf4c63a1c8e342fa5ea64ddc51643
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-01 20:32:41 +0530

    kid 1-5.c

---
+++ kid 1-5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+printf("enter the n");
+scanf("%d%d%d,&x&y&z);
+if(x>y x>z)
+{
+printf("/n %d greatest number",x);
+elseif(y>z)
+{
+printf("/n %d greatest number",y);
+}
+else
+{
+printf("/n %d greatest number",z);
+}
