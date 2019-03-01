4c24fd8f3f043d403600b1f1bebf9a53791fac87
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-01 20:19:24 +0530

    kid 1-4.c

---
+++ kid 1-4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x;
+printf("enter n");
+scanf("%c",&x);
+if(x>=a&&x<=z||x>A&&x<Z)
+{
+printf("/n yes is a alphabet x");
+}
+else
+{
+printf("/n no is not a alpabet");
+}
