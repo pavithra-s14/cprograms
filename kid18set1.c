98c76adb4334e99faa29c0dbdaf9dd519eaccf85
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-09 20:52:21 +0530

    kid18set1.c

---
+++ kid18set1.c
@@ -0,0 +1,17 @@
+#include<stdio,h>
+incluse<conio.h>
+void main()
+{
+int n,t,r,s=0;
+printf("enter the number",n);
+scanf("%d",&n);
+while(n>0)
+{
+r=t%d;
+s=s+(r*r*r);
+t=t/10;
+}
+printf("amrstrong no is %d,s");
+
+getch();
+}
