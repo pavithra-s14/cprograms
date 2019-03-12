fd6a577b9115ce48b3941a7b1ffe0d06796d33ef
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-12 20:21:56 +0530

    kidset11set1.c

---
+++ kidset11set1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+int i,x,y,pow=1;
+printf("enter the base",x);
+scanf("%d",&x);
+printf("enter the index",y);
+scanf("%d",&y);
+for(i=1;i<=n;i++)
+{
+pow=pow*x;
+}
+printf("/n %d ^%d=%d,x,y,pow);
+}
