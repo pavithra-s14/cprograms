820af8bc931de5aba909fa7ef94a45f3b8c50893
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-12 19:57:57 +0530

    kid13set1.c

---
+++ kid13set1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,i,count=0;
+printf("enter the two number");
+scanf("%d",&n);
+for(i=2;i<=n;i++)
+{
+if(n%2==0)
+{
+c++;
+}
+}
+if(c==0)
+{
+printf("/n%d enter the prime number",n);
+}
+else
+{
+printf("/n is not a primenumber",n);
+}
