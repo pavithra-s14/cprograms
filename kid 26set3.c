909c926e6d3226bbd0f75e006e3b00e2497f926a
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-13 19:39:51 +0530

    kid26set 3.c

---
+++ kid 26set3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,n,a[10],sum=0;
+int avg=0;
+for(i=0;i<=n;i++)
+{
+printf("enter the numbers %d",i++);
+scanf("%d",&a[i]);
+sum=sum+a[10];
+}
+}
+for(i=0;i<=n;i++)
+{
+avg=sum/n;
+printf("%d",&n);
+}
