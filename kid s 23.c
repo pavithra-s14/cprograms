1060603d5693ab958487cf9e54d1c466047dc313
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-13 16:20:22 +0530

    kid s 23.c

---
+++ kid s 23.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+int a[10];
+int i,j,n,temp;
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=0;j<n-1;j++)
+{
+if(a[j]=a[j+1];
+a[j+1]=tempn;
+}
+}
+}
+}
+for(i=0;i<=n;i++)
+{
+printf("%d/n",a[i]);
+}
+}
