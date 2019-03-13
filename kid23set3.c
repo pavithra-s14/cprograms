54509744b9d582eb44206eaab54f1248c57755fa
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-13 19:53:37 +0530

    kid 23 set3.c

---
+++ kid23set3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,j,min;
+int a[n];
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+min=a[0];
+}
+for(i=0;i<=n;i++)
+{
+if(min>a[i])
+min=a[i]}
+printf("%d",&a[i])
+return 0;
+}
