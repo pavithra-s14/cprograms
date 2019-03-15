4d437deb873c228fde1ecb5205f83d03065ba6dd
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-15 15:50:31 +0530

    kid42 s 5.c

---
+++ kid42 s 5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+char str1[50],str2[50];
+int n1,n2;
+printf("enter the string1");
+scanf("%s",s);
+printf("enter the string2");
+scanf("%s",s);
+n1=strlen(str1);
+n2=strlen(str2);
+if(n>n2)
+{
+printf("\n%s",str1);
+}
+else if(n2>n1)
+{
+printf("\n%s",str2);
+}
+else
+{
+printf("\n%s",s);
+}
+return 0;
+}
