e44f2cf8c72161e9060a6503cf3a040bab789344
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-15 15:32:26 +0530

    kid 41 s 5.c

---
+++ kid41 s 5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char s[50];
+int k,i;
+printf("enter the string");
+scanf("%s",s);
+printf("enter the values");
+scanf("%d",&k);
+for(i=1;i<10;i++)
+{
+printf("/ns",&s);
+}
+return 0;
+}
