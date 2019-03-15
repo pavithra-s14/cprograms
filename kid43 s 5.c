6cd980427f13411ec94cfdac3992cd5c0e3af626
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-15 16:09:49 +0530

    kid43 s 5.c

---
+++ kid43 s 5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str1[50],str[50];
+int i,j;
+printf("\n enter the string1:");
+scanf("%s,s);
+printf("\n enter the string2:");
+scanf("%s,s);
+for(i=0;str1![i]='\0';i++)
+{
+}
+for(j=0;str2![j]='\0';j++)
+{
+str1[i]=str[j];
+i++;
+}
+str1!='\0';
+printf("\n%s,str1);
+return 0;
+}
