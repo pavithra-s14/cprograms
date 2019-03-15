f61be35aa8b6df6cb07e6477cf003c58f70eb0f0
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-15 20:41:54 +0530

    kid47 s 5.c

---
+++ kid47 s 5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main();
+int array[100];
+int i,n;
+printf("enter the size of array");
+scanf("%d",&n);
+printf("enter the elements");
+for(i=0;i<=n;i++)
+{
+scanf(%d",&array[i];
+}
+intmax=array[0];
+intmin=array[0];
+for(i=1;i<=n;i++)
+{
+if(array[i]>max)
+{
+max=array[i];
+}
+ifarray[i]<max)
+{
+min=arrar[i];
+}
+printf("\n%dmin);
+printf("\n%dmax);
+return 0;
+}
