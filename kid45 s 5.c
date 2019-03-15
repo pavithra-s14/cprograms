152e2249e75dc4c24c119d7349b6db9e30b6fc86
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-15 20:02:36 +0530

    kid45 s 5.c

---
+++ kid45 s 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[50];
+int i,count=0;
+for(i=0;str[i]!=\'0';i++)
+{
+str[i]>='0'&&str[i]<='9';
+{
+count=count+1;
+}
+}
+printf("\n%d",count);
+return 0;
+}
