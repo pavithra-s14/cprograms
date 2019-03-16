2779ab474f99e69c4c14b9b926aac1bf42713cb9
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-16 19:41:40 +0530

    kid48 s 5.c

---
+++ kid48 s 5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,i,sum-0,avg;
+int array[50];
+printf("enter the size of array");
+scanf("%d",&array[i]);
+printf("enter the array elements");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&array[i]);
+}
+for(i=0;i<=n;i++)
+{
+sum=sum+array[i];
+}
+avg=sum\n
+printf("\n%d",sum);
+printf("\n%d",avg);
+return 0;
+}
