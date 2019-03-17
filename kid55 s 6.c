e6429f50c3e8c53b720b8444de40c347faaba55c
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-17 20:56:59 +0530

    kid55 s  6.c

---
+++ kid55 s 6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int num,n,m,pro;
+printf("enter the number1");
+scanf("%d",&even);
+printf("enter the number2");
+scanf("%d",&one);
+pro=m*n;
+if(pro%2==0)
+{
+printf("\neven");
+}
+else
+{
+printf("\nodd");
+}
+return 0;
+}
