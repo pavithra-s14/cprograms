8779192f827269d6ca11004fb1af68dc2a5cf37e
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-17 20:16:44 +0530

    kid 53 s 6.c

---
+++ kid53 s 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int number,temp,sum=0;
+printf("\enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+temp=num%10;
+sum=sum+10;
+n=n\10;
+}
+printf("\n%d",number");
+return 0;
+}
