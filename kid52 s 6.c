daa0c5b7fe1601294367d230c692eedd3a74a5ef
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-17 19:57:37 +0530

    kid52 s 6.c

---
+++ kid52 s 6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("\nenter the number from 1 to 10");
+scanf("%d",&n);
+switch(number):
+{
+case 1:
+printf("\none");
+break;
+case 2:
+printf("\two");
+break;
+case 3:
+printf("\nthree");
+break;
+case 4:
+printf("\nfour");
+break;
+case 5:
+printf("\nfive");
+break;
+case 6:
+printf("\nsix");
+break;
+case 7:
+printf("\nseven");
+break;
+case 8printf("\neight");
+break;
+case 9:
+printf("\nnine");
+break;
+case 10:
+printf("\nten");
+break;
+default:
+printf("\n enter the numbers from 1 to 10");
+}
+return 0;
+}
