5b363e36e154ca0da94df787da1a9bc8c32e7c0e
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-19 10:35:02 +0530

    kid63.c

---
+++ kid63.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+int m,i,count=0;
+scanf("%[^\t\n]s",a);
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i]=='');
+}
+else
+{
+continue;
+}
+}
+printf("%d",count+1)
+return 0;
+}
