3063064aaa21bf2064ff2cdd3959edfabe769bcd
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-19 10:15:25 +0530

    kid62.c

---
+++ kid62.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int i,m,flag=0;
+m=strlen(a);
+scanf("%s",a);
+for(i=0;i<m;i++)
+{
+if
+{
+ifa[i]==1||a[i]==0;
+{
+flag=1;
+}
+else
+{
+flag=0;
+break;
+}
+}
+if(flag==1)
+{
+printf("yes");
+}
+}
