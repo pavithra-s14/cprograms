91eb4eb4ff5b6847ae14a99621b136617b844a6c
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-19 09:55:14 +0530

    kid61.c

---
+++ kid61.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[20];
+int k,i,;
+printf("/n enter the k values");
+scanf("%d%s",&k,str);
+for(i=0;i<k;i++)
+{
+printf("/n%c",str[i]);
+return 0;
+}
