089cfa042ede432121762add01ca30adefb7d90c
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-21 19:30:16 +0530

    kid80.c

---
+++ kid80.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int num,rev,rem;
+while(n!=0)
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+num=rev;
+while(n!=0)
+{
+rem=n%10;
+if(rem%2==0)
+{
+printf("%d",rem);
+}
+return 0;
+}
