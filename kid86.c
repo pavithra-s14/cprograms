b8f4a0822780d4d32165ec02ce29fda12d89705b
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-22 19:53:15 +0530

    kid86.c

---
+++ kid86.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int n,i,j,count=o;
+scanf("%s",s);
+n=strlen(a);
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+ifa[i]=b[i]
+{
+count=1;
+break;
+}
+else
+{
+continue;
+}
+}
+}
+if(count==0)
+{
+printf("yes...isogram")
+}
+else
+{
+printf("no");
+}
+return 0;
+}
