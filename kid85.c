c7e0ef5bd8cd231480a7d5040882669ccad1a4b5
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-22 19:31:39 +0530

    kid85.c

---
+++ kid85.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int i,n;
+scanf("%s",s);
+n=strlen(s);
+for(i=0;i<n;i++)
+{
+if(n%2==0)
+{
+printf("%c",s[i]);
+}
+}
+for(i=0;i<=n;i++)
+{
+if(n%2!==0)
+{
+printf("%c",s[i]);
+}
+}
+return 0;
+}
