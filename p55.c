4b3012e46cced45d2d83007911c917673ac69769
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-24 10:02:46 +0530

    p55.c

---
+++ p55.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sting.h>
+int main()
+{
+char a[20],b[20];
+int i,j;
+scanf("%s"%s,a,b);
+for(i=0;str[i]!=0;j=0;str[j]!=0;i++,j++)
+{
+if(a[i]=b[i]);
+{
+count=1;
+}
+else
+{
+printf("no");
+count=0;
+break;
+}
+}
+if(count==1)
+{
+printf("yes both the strings are same");
+}
+return 0;
+}
