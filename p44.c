a172bd7c10f34e00066863721236971fcb7e7a10
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-24 09:11:16 +0530

    p44.c

---
+++ p44.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[20];
+scanf("%s",a);
+int n=strlen(a);
+int k;
+for(i=0;i<k;i++)
+{
+char temp a[0];
+int j;
+for(j=0;j<n-1;j++)
+{
+a[j]=a[j+1];
+}
+a[j]=temp;
+}
+printf("%s",a);
+return 0;
+}
